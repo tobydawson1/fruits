9cfb3fa6ef161d946f71b61d0090ff18f34da756
Richard Pattinson <richie.pattinson@googlemail.com>
2020-03-17 16:11:47 +0000

    working crud app with fruits

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -4,7 +4,69 @@ class Body extends React.Component {
         this.state = {
           fruits: []
         };
+        this.handleFormSubmit = this.handleFormSubmit.bind(this)
+        this.addNewFruit = this.addNewFruit.bind(this)
+        this.handleDelete = this.handleDelete.bind(this)
+        this.deleteFruit = this.deleteFruit.bind(this)
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.updateFruit = this.updateFruit.bind(this)
       }
+    handleUpdate(fruit){
+      fetch(`http://localhost:3000/api/v1/fruits/${fruit.id}`, 
+      {
+        method: 'PUT',
+        body: JSON.stringify({fruit: fruit}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((response) => { 
+          this.updateFruit(fruit)
+        })
+    }
+    updateFruit(fruit){
+      let newFruits = this.state.fruits.filter((f) => f.id !== fruit.id)
+      newFruits.push(fruit)
+      this.setState({
+        fruits: newFruits
+      })
+    }
+    handleDelete(id){
+      fetch(`http://localhost:3000/api/v1/fruits/${id}`, 
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((response) => { 
+        this.deleteFruit(id)
+      })
+    }
+    deleteFruit(id){
+      newFruits = this.state.fruits.filter((fruit) => fruit.id !== id)
+      this.setState({
+        fruits: newFruits
+      })
+    }
+
+    handleFormSubmit(name, description){
+      console.log(name, description)
+      let body = JSON.stringify({fruit: {name: name, description:   description} })
+      fetch('http://localhost:3000/api/v1/fruits', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    }).then((response) => {return response.json()})
+    .then((fruit)=>{
+      this.addNewFruit(fruit)
+    })
+    }
+    addNewFruit(fruit){
+      this.setState({
+        fruits: this.state.fruits.concat(fruit)
+      })
+    }
     componentDidMount(){
         fetch('/api/v1/fruits.json')
           .then((response) => {return response.json()})
@@ -13,8 +75,9 @@ class Body extends React.Component {
     render(){
         return(
           <div>
-            < NewFruit /> 
-            <AllFruits fruits={this.state.fruits} />
+            < NewFruit handleFormSubmit={this.handleFormSubmit}/> 
+            <AllFruits fruits={this.state.fruits} handleDelete={this.handleDelete} handleUpdate = {this.handleUpdate}/>
+
           </div>
         )
       }
--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -2,7 +2,7 @@ const NewFruit = (props) => {
     let formFields = {}
    
     return(
-      <form>
+        <form onSubmit={ (e) => { props.handleFormSubmit(formFields.name.value, formFields.description.value); e.target.reset();}}>
        <input ref={input => formFields.name = input} placeholder='Enter the name of the item'/>
        <input ref={input => formFields.description = input} placeholder='Enter a description' />
        <button>Submit</button>
