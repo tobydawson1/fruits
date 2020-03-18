f89dce9ca1efcbbf7bebc8368d9b3b6659aced45
Student <student@makerss-air.home>
2020-03-18 14:37:23 +0000

    add date and time in the post

--- app/assets/javascripts/components/_new_post.js.jsx
+++ app/assets/javascripts/components/_new_post.js.jsx
@@ -3,7 +3,7 @@ const NewPost = (props) => {
    
     return(
     <form onSubmit={ (e) => { props.handleFormSubmit(formFields.message.value); e.target.reset();}}>
-       <input ref={input => formFields.message = input} placeholder='Enter your post' />
+       <textarea ref={input => formFields.message = input} placeholder='Enter your post' />
        <button>Submit</button>
       </form>
     )
