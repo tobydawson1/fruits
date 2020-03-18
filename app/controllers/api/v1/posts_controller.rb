dceaeaca32ed5c8ac5f4bc76956880db12c47a51
Student <student@makerss-air.home>
2020-03-18 10:51:52 +0000

    add post_controller

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::FruitsController < ApplicationController
+    
+    def index
+        render json: Post.all
+    end
+
+    def create
+        post = Post.create(post_params)
+        render json: post
+    end
+
+    def destroy
+        Post.destroy(params[:id])
+    end
+
+    def update
+        post = Post.find(params[:id])
+        post.update_attributes(post_params)
+        render json: post
+    end
+
+    private
+    # check current user_id
+    def post_params
+        params.require(:post).permit(:message, :user_id)
+    end
+
+end
